1556f226a997f6d7f86f4725097eb7f9ff07f759
dsgnmike <mishaborisov2013@gmail.com>
2019-01-06 22:52:35 +0300

    Initial Commit

---
+++ Tinkoff_News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff_News
+//
+//  Created by Михаил Борисов on 06/01/2019.
+//  Copyright © 2019 Михаил Борисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tinkoff_NewsTests/Tinkoff_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tinkoff_NewsTests.swift
+//  Tinkoff_NewsTests
+//
+//  Created by Михаил Борисов on 06/01/2019.
+//  Copyright © 2019 Михаил Борисов. All rights reserved.
+//
+
+import XCTest
+@testable import Tinkoff_News
+
+class Tinkoff_NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
